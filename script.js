f4ecec8529449d8093ef1907a9178219f0fe3ac9
Koray Kolburan <80157575+koraykolburan@users.noreply.github.com>
2021-10-02 19:00:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("side-menu").style.width = "300px";
+  document.getElementById("content-area").style.marginTop = "300px";
+}
+function closeNav() {
+  document.getElementById("side-menu").style.width = "0";
+  document.getElementById("content-area").style.marginTop = "0";
+}
